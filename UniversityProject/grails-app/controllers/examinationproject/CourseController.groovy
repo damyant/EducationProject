5d23e51ef51b247520a1f5c108477e8579c515e7
sonaligupta <sonaligupta847@gmail.com>
2014-03-14 13:49:15 +0530

    updated

--- UniversityProject/grails-app/controllers/examinationproject/CourseController.groovy
+++ UniversityProject/grails-app/controllers/examinationproject/CourseController.groovy
@@ -15,9 +15,6 @@ def createNewCourse() {
     def multiSelect(){
         def list1 = Subject.findAll()
         def list2 = []
-
-        println "Rendering List1 : "+list1
-        println "Rendering List2 : "+list2
         [index:params.int('index'), list1:list1, list2:list2]
 
     }
