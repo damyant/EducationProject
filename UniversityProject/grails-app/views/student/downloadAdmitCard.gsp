43376f48b47a28fd403f2300c2b8729142f8f523
sonaligupta <sonaligupta847@gmail.com>
2014-04-25 13:31:39 +0530

    Merge branch 'damyant' of https://github.com/damyant/UniversityProject into damyant
    Conflicts:
    	UniversityProject/grails-app/controllers/com/university/AdminController.groovy
    	UniversityProject/grails-app/controllers/examinationproject/StudentController.groovy
    	UniversityProject/grails-app/views/admin/feeVoucher.gsp
    	UniversityProject/web-app/js/registerPage.js

--- UniversityProject/grails-app/views/student/downloadAdmitCard.gsp
+++ UniversityProject/grails-app/views/student/downloadAdmitCard.gsp
@@ -13,22 +13,29 @@
 </head>
 
 <body>
+<div id="main">
+    <fieldset class="form">
+        <h3>Download Admit Card</h3>
 <g:if test="${flash.message}">
     <div class="error">${flash.message}</div>
 </g:if>
 <g:form name="abc1" id="abc1" controller="admitCard" action="printAdmitCard">
+    <div class="">
     <p><bold>Please fill the information to download the Admit Card</bold></p>
-    <div class="input">
-        <input type="text" name="rollNumber" id="rollNo"  placeholder="Roll Number"/><label id="rollMsg" class="error"></label>
-    </div>
-    <div class="input">
-        <input type="text" name="dob" id="dob" placeholder="Date of Birth" /><label id="dobMsg"></label>
-    </div>
-    <input type="button"  value="Submit" onclick="downloadAdmitCard();" />
 
+    <div class="input university-bottom-margin">
+        <label for="rollNo" class="university-right-margin">Enter Roll Number</label><input type="text" name="rollNumber" id="rollNo" class="university-size-1-3"  placeholder="Roll Number"/><label id="rollMsg" class="error"></label>
+    </div>
+    <div class="input university-bottom-margin">
+        <label for="dob" class="university-right-margin">Enter Date of Birth</label><input type="text" class="university-size-1-3" name="dob" id="dob" placeholder="Date of Birth" /><label id="dobMsg"></label>
+    </div>
+    <input type="button"  value="Submit" class="ui-button" onclick="downloadAdmitCard();" />
+    </div>
 %{--<div id="statusofApp" style="display: none">--}%
 
 %{--</div>--}%
 </g:form>
+        </fieldset>
+    </div>
 </body>
 </html>
--- UniversityProject/web-app/js/registerPage.js
+++ UniversityProject/web-app/js/registerPage.js
@@ -1,12 +1,10 @@
+function uploadImage(input, type) {
+    $("#profile-image-upload").change(function () {
+        //  var iSize = ($("#profile-image-upload")[0].files[0].size / 1024);
 
-function uploadImage(input,type){
-    $("#profile-image-upload").change(function ()
-    {
-      //  var iSize = ($("#profile-image-upload")[0].files[0].size / 1024);
+        if (iSize <= 50) {
 
-        if(iSize<=50){
-
-    }
+        }
 
 //        if (iSize / 1024 > 1)
 //        {
@@ -33,47 +31,45 @@ function uploadImage(input,type){
 }
 
 
-
-
 function isNumber(evt) {
 
     evt = (evt) ? evt : window.event;
     var charCode = (evt.which) ? evt.which : evt.keyCode;
     if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-    return false;
+        return false;
     }
-return true;
+    return true;
 }
 
-$('#profile-image').on('click', function() {
+$('#profile-image').on('click', function () {
 //    alert("click")
     $('#profile-image-upload').click();
 });
 
 
-function readURL(input,type) {
+function readURL(input, type) {
     if (input.files && input.files[0]) {
         var FileUploadPath = $("#profileImage").val()
         var Extension = FileUploadPath.substring(FileUploadPath.lastIndexOf('.') + 1).toLowerCase();
-        var imgkbytes = Math.round(parseInt(input.files[0].size)/1024)
-        if(imgkbytes<=50 && (Extension == "gif" || Extension == "png" || Extension == "bmp" || Extension == "jpeg"|| Extension == "jpg")){
-       var reader = new FileReader();
-        if(type=='picture')
-            reader.onload = function (e) {
-                $('#picture')
-                    .attr('src', e.target.result)
-                    .width(150)
-                    .height(200);
-            };
-        if(type=='signature')
-            reader.onload = function (e) {
-                $('#signature')
-                    .attr('src', e.target.result)
-                    .width(250)
-                    .height(80);
-            };
+        var imgkbytes = Math.round(parseInt(input.files[0].size) / 1024)
+        if (imgkbytes <= 50 && (Extension == "gif" || Extension == "png" || Extension == "bmp" || Extension == "jpeg" || Extension == "jpg")) {
+            var reader = new FileReader();
+            if (type == 'picture')
+                reader.onload = function (e) {
+                    $('#picture')
+                        .attr('src', e.target.result)
+                        .width(150)
+                        .height(200);
+                };
+            if (type == 'signature')
+                reader.onload = function (e) {
+                    $('#signature')
+                        .attr('src', e.target.result)
+                        .width(250)
+                        .height(80);
+                };
         }
-        else{
+        else {
             $("#profileImage").val('')
             $("#picture").attr('src', ' ')
             alert("Please upload an image of size less then 50kb and in allowed format")
@@ -84,28 +80,27 @@ function readURL(input,type) {
 }
 
 
-jQuery(function($) {
-    $( "#datePick" ).datepicker({
+jQuery(function ($) {
+    $("#datePick").datepicker({
         changeMonth: true,
         changeYear: true,
         dateFormat: "mm/dd/yy",
         maxDate: 0
     });
 
-    $( "#datePick1" ).datepicker({
+    $("#datePick1").datepicker({
         changeMonth: true,
         changeYear: true,
         dateFormat: "mm/dd/yy",
         maxDate: 0
     });
-    $( "#admissionDate" ).datepicker({
+    $("#admissionDate").datepicker({
         changeMonth: true,
         changeYear: true,
         dateFormat: "mm/dd/yy",
         maxDate: 0
     });
 });
-<<<<<<< HEAD
 function checkApplicationNumber(t) {
 
     var data = $(t).val();
@@ -127,10 +122,10 @@ function checkApplicationNumber(t) {
         }
     });
 }
-function submitTempRegistration(){
+function submitTempRegistration() {
     validate();
-    var result= $('#tempEnrollment').valid()
-    if(result) {
+    var result = $('#tempEnrollment').valid()
+    if (result) {
         $.ajax({
             type: "post",
             url: url('student', 'tempRegistration', ''),
@@ -143,30 +138,17 @@ function submitTempRegistration(){
         });
     }
 }
-//jQuery(function($) {
-    function confirmGenerateChallan(rollno) {
+function confirmGenerateChallan(rollno) {
 
-            var result=confirm("Do you want to Generate Challan for Roll No "+rollno+" ?");
-            if (result==true)
-            {
+    var result = confirm("Do you want to Generate Challan for Roll No " + rollno + " ?");
+    if (result == true) {
 //              window.open('/UniversityProject/admin/generateFeeVoucher/?rollNo='+rollNo+'&feeType='+1);
-                window.location.href = '/UniversityProject/admin/feeVoucher?rollNo=' + rollno;
-            }
-            else
-            {
-
-                window.location.href = '/UniversityProject/student/enrollmentAtIdol';
-                $("#errorMessage").text('Student Registered Successfully & Roll No is '+rollno)
-            }
+        window.location.href = '/UniversityProject/admin/feeVoucher?rollNo=' + rollno;
     }
-=======
+    else {
 
+        window.location.href = '/UniversityProject/student/enrollmentAtIdol';
+        $("#errorMessage").text('Student Registered Successfully & Roll No is ' + rollno)
+    }
+}
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 1a6324c5addb2454551b33030f90b8a002bc0662
-
-
-
->>>>>>> ac8c36f577e9041e428ac23b1a2e881aaad9733d
