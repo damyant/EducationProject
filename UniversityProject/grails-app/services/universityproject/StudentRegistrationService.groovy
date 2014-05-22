015b92bb461d775b272658e45dc999bba2fd6855
rajpdamyant <rajp@damyant.com>
2014-05-22 14:36:36 +0530

    update project

--- UniversityProject/grails-app/services/universityproject/StudentRegistrationService.groovy
+++ UniversityProject/grails-app/services/universityproject/StudentRegistrationService.groovy
@@ -87,8 +87,6 @@ class StudentRegistrationService {
 
         }
 
-
-
         studentRegistration.programSession = programSessionIns
         studentRegistration.programDetail = programDetail
         if(params.idol=="idol")
