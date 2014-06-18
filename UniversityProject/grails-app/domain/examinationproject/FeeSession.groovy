a09fcf446f8efd0040702b074c1541e5f94715e3
Ajay <ajays@damyant.com>
2014-06-18 18:28:00 +0530

    task

---
+++ UniversityProject/grails-app/domain/examinationproject/FeeSession.groovy
@@ -0,0 +1,21 @@
+package examinationproject
+
+class FeeSession {
+    ProgramDetail programDetailId
+    String sessionOfFee
+
+    static hasMany = [admissionFee: AdmissionFee,miscellaneousFee:MiscellaneousFee]
+    static constraints = {
+        sessionOfFee(nullable: true)
+    }
+
+
+    static mapping = {
+        sessionOfFee column: "sessionOfFee"
+        programDetailId column: "ProgramDetailId"
+        admissionFee column: "AdmissionFeeId"
+        miscellaneousFee column: "MiscellaneousFeeId"
+        admissionFee cascade:"all,delete-orphan"
+        miscellaneousFee cascade:"all,delete-orphan"
+    }
+}
