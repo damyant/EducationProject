a4fbcfe7d9d4092fcaf914134212afca5cd8dc41
chandandamyant <chandans@damyant.com>
2014-08-19 11:00:14 +0530

    all

--- UniversityProject/grails-app/controllers/postexamination/PostExaminationController.groovy
+++ UniversityProject/grails-app/controllers/postexamination/PostExaminationController.groovy
@@ -240,7 +240,7 @@ class PostExaminationController {
     }
     def xmlParseData = {
         XmlParser parser = new XmlParser()
-        def xmldata1 = parser.parse (new FileInputStream("web-app/ruleXml/marksRule.xml"))
+        def xmldata1 = parser.parse (new FileInputStream("web-app/marks/passMarks.xml"))
         println(""+xmldata1)
         def allRecords = xmldata1.program.semester.size()
         println(allRecords)
